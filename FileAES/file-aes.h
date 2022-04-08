2d78819f7a663e5decebcaa6749ae5191687e994
Roy <iqosjay@gmail.com>
2022-04-09 05:47:29 +0800

    key 获取成功后存堆内存（之前是有Bug的）

---
+++ FileAES/file-aes.h
@@ -0,0 +1,59 @@
+﻿#ifndef FILE_AES_WINDOWS_FILE_AES_H_
+#define FILE_AES_WINDOWS_FILE_AES_H_
+
+#include <cstdio>
+
+#include "aes.h"
+#include "definitions.h"
+
+namespace roy {
+
+// 操作模式
+enum class Mode {
+  kEncrypt = 0,  // 加密
+  kDecrypt,      // 解密
+  kUnspecified   // 无效
+};
+
+typedef struct AESPARAMS {
+  Mode mode_ = Mode::kUnspecified;  // 操作模式
+  uint8_t* key_ = 0;                // 加（解）密的 key
+  string src_path_ = "";            // 输入文件路径
+  string out_path_ = "";            // 输出文件路径
+  ~AESPARAMS() { SAFE_DELETE_BUF(key_); }
+} AESParams;
+
+class FileAES {
+ public:
+  // 获取AES文件加解密参数
+  static AESParams* FetchParams(int argc, char** argv);
+
+  // 加密文件
+  // infile  : 输入文件
+  // outfile : 输出文件
+  // key     : 加密的 key
+  // return  : 当且仅当返回0的时候表示文件加密操作成功
+  static int EncryptFile(const char* infile, const char* outfile,
+                         const uint8_t* key);
+
+  // 解密文件
+  // infile  : 输入文件
+  // outfile : 输出文件
+  // key     : 解密的 key
+  // return  : 当且仅当返回0的时候表示文件解密操作成功
+  static int DecryptFile(const char* infile, const char* outfile,
+                         const uint8_t* key);
+
+ private:
+  FileAES();
+  ~FileAES();
+
+ private:
+  static uint32_t Byte2ToInt(uint8_t*);
+  static void IntToBytes(uint32_t, uint8_t*);
+};
+}  // namespace roy
+
+#endif  // FILE_AES_WINDOWS_FILE_AES_H_
+
+// TODO: 在此处引用程序需要的其他标头。
--- FileAES/main.cpp
+++ FileAES/main.cpp
@@ -1,67 +1,64 @@
 #include <io.h>
-#include "FileAes.h"
+
+#include <iostream>
+
+#include "file-aes.h"
+
 #define AES_DEFAULT_KEY "ABCDEFGHIJKLMNOP"
-int main(int argc, char* argv[])
-{
-	using std::cout;
-	using std::endl;
-	cout << "主函数收到的参数：" << endl;
-	for (int i = 0; i != argc; ++i)
-	{
-		cout << "argv[" << i << "] = " << argv[i] << endl;
-	}
-	Roy::AESParams* ptr = Roy::FileAES::FetchParams(argv, argc);
-	if (!ptr)
-	{
-		cout << "获取参数失败！" << endl;
-		return 1;
-	}
-	Roy::Mode mode = ptr->mode;
-	Roy::String fin = ptr->srcPath;
-	Roy::String fout = ptr->outPath;
-	uint8_t* key = ptr->key;
-	delete (ptr);
-	ptr = NULL;
-	if (fin.empty())
-	{
-		cout << "输入文件（fin）未指定！" << endl;
-		return 2;
-	}
-	if (fout.empty())
-	{
-		cout << "输出文件（fout）未指定！" << endl;
-		return 3;
-	}
-	const char* outPath = fout.c_str();
-	if (0 == _access(outPath, 0))
-	{
-		if (0 != remove(outPath))
-		{
-			cout << "输出文件存在同名文件！且删除失败！" << endl;
-			return -1;
-		}
-	}
-	if (0 == key)
-	{
-		key = (uint8_t*)AES_DEFAULT_KEY;
-	}
-	int code;
-	switch (mode)
-	{
-	case Roy::Mode::Encrypt:
-		code = Roy::FileAES::EncryptFile(fin.c_str(), fout.c_str(), key);
-		break;
-	case Roy::Mode::Decrypt:
-		code = Roy::FileAES::DecryptFile(fin.c_str(), fout.c_str(), key);
-		break;
-	case Roy::Mode::Unspecified:
-	default:
-		cout << "Mode未指定！" << endl;
-		code = 2;
-	}
-	if (0 == code)
-	{
-		cout << ((mode == Roy::Mode::Encrypt) ? "加密完成！" : (mode == Roy::Mode::Decrypt) ? "解密完成！" : "") << endl;
-	}
-	return code;
+
+int main(int argc, char* argv[]) {
+  using std::cout;
+  using std::endl;
+  cout << "主函数收到的参数：" << endl;
+  for (int i = 0; i != argc; ++i) {
+    cout << "argv[" << i << "] = " << argv[i] << endl;
+  }
+  auto params = roy::FileAES::FetchParams(argc, argv);
+  if (nullptr == params) {
+    cout << "获取参数失败！" << endl;
+    return 1;
+  }
+  roy::Mode mode = params->mode_;
+  roy::string fin = params->src_path_;
+  roy::string fout = params->out_path_;
+  uint8_t* key = params->key_;
+  SAFE_DELETE_PTR(params);
+  if (fin.empty()) {
+    cout << "输入文件（fin）未指定！" << endl;
+    return 2;
+  }
+  if (fout.empty()) {
+    cout << "输出文件（fout）未指定！" << endl;
+    return 3;
+  }
+  const char* out_path = fout.c_str();
+  if (0 == _access(out_path, 0)) {
+    if (0 != remove(out_path)) {
+      cout << "输出文件存在同名文件！且删除失败！" << endl;
+      return -1;
+    }
+  }
+  if (0 == key) {
+    key = (uint8_t*)AES_DEFAULT_KEY;
+  }
+  int code;
+  switch (mode) {
+    case roy::Mode::kEncrypt:
+      code = roy::FileAES::EncryptFile(fin.c_str(), fout.c_str(), key);
+      break;
+    case roy::Mode::kDecrypt:
+      code = roy::FileAES::DecryptFile(fin.c_str(), fout.c_str(), key);
+      break;
+    case roy::Mode::kUnspecified:
+    default:
+      cout << "Mode未指定！" << endl;
+      code = 2;
+  }
+  if (0 == code) {
+    cout << ((mode == roy::Mode::kEncrypt)   ? "加密完成！"
+             : (mode == roy::Mode::kDecrypt) ? "解密完成！"
+                                             : "")
+         << endl;
+  }
+  return code;
 }
