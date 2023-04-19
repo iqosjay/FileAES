181bea92c1a3f25ecf4b8478143d74581cb822d9
Roy <iqosjay@gmail.com>
2023-04-19 21:26:39 +0800

    fix bug

---
+++ FileAES/definitions.h
@@ -0,0 +1,14 @@
+#ifndef BR_ACTIVE_DEFINITIONS_H_
+#define BR_ACTIVE_DEFINITIONS_H_
+
+#include <cstdint>
+
+namespace roy {
+
+typedef int32_t int32;
+typedef uint8_t uint8;
+typedef uint32_t uint32;
+
+}
+
+#endif//BR_ACTIVE_DEFINITIONS_H_
