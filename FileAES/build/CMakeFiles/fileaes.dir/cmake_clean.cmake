5e09818bea3a024590c637debeb6cab49d6c115f
Roy <iqosjay@gmail.com>
2023-04-19 21:28:28 +0800

    add compiled binary

---
+++ FileAES/build/CMakeFiles/fileaes.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fileaes.dir/cipher/aes.c.o"
+  "CMakeFiles/fileaes.dir/cipher/aes.c.o.d"
+  "CMakeFiles/fileaes.dir/cipher/file-aes.cpp.o"
+  "CMakeFiles/fileaes.dir/cipher/file-aes.cpp.o.d"
+  "CMakeFiles/fileaes.dir/main.cpp.o"
+  "CMakeFiles/fileaes.dir/main.cpp.o.d"
+  "CMakeFiles/fileaes.dir/util/logger.cpp.o"
+  "CMakeFiles/fileaes.dir/util/logger.cpp.o.d"
+  "fileaes"
+  "fileaes.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/fileaes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
